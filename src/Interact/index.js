9c32423a650ccd7b019dd52a8755856891dbb52a
ProbablyX <83953506+davdtheemonk@users.noreply.github.com>
2022-05-02 22:21:13 +0300

    Update index.js

--- src/Interact/index.js
+++ src/Interact/index.js
@@ -9,7 +9,7 @@ export default function Interact(){
                 <p className="interact_textp1">INTERACTIVE</p>
                 <h3>Interactive tools</h3>
                 <p className="interact_textp2">
-                    The platform is designed to offer various tools that foster interaction.Tool include video confrencing,student or users collaboration and live coding assesments and courses.
+                    The platform is designed to offer various tools that foster interaction.Tools include video confrencing,student or users collaboration and live coding assesments and courses.
                 </p>
                 <hr></hr>
                 <Link to="/portal">Try Now -></Link>
