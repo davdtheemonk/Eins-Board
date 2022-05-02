d1c7c3637213ba308c16631387f53fb73d3a9d5a
davdtheemonk <parishniclaus@gmail.com>
2022-05-02 21:49:01 +0300

    new

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import { Route,Routes, BrowserRouter } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-
-
+import Home from "./Home"
+import Template from "./Template"
+import Talkpython from './Talkpython'
 import Docs from "./Docs"
 import Dashboard  from './Dashboard';
 import Signup from "./Signup"
 import Login from "./Login"
-
+import Course from "./Course"
+import PythonMore from "./PythonMore"
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import Hire from "./Hire"
@@ -32,7 +34,7 @@ ReactDOM.render(
   <BrowserRouter>
   <Routes>
     <Route path='/' element={<Hire />}/>
-  
+   <Route path='/community' element={<Talkpython></Talkpython>}/>
     <Route path='/documentation' element={<Docs />}/>
  <Route path='/portal' element={<Dashboard />}/>
     <Route path='/portal/ws/:id' element={<Dashboard />}/>
